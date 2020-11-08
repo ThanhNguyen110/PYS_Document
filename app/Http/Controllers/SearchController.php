f6ed8070fc784b3a7c3fa747a080705bd7f80b97
ThanhNguyen110 <thanhnv.hn110@gmail.com>
2020-11-08 22:15:17 +0700

    updated

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,29 +11,28 @@ class SearchController extends Controller
 {
   public function list(Request $request)
   {
-    $categories = Category::where('parent_id', null)->get();
     $keyword = $request->input("search");
     $posts   = Post::where('title', 'LIKE', '%' . $keyword . '%')
       ->where('status', 'on')
       ->get();
 
     if ($request->ajax()) {
-      $output  = '';
-
       if (count($posts) > 0) {
-        $output = '<ul class="list-group d-block position-absolute">';
+        $output = "<ul class='list-group d-block position-absolute'>";
         foreach ($posts as $p) {
-          $subCategory = Category::where('id', $p->category_id)->first();
-          $mainCategory = Category::where('id', $subCategory->parent_id)->first();
           $output .=
-            "<li class='list-group-item'>" . $p->title . '</a></li>';
+            "<li class='list-group-item'>"
+            . $p->title .
+            "</li>";
         }
         $output .= '</ul>';
       } else {
-        $output .=
-          '<ul class="list-group position-absolute">
-            <li class="list-group-item">' . 'Không tìm thấy bài viết' . '</li>
-          </ul>';
+        $output =
+          "<ul class='list-group position-absolute'>" .
+          "<li class='list-group-item'>" .
+          "Không tìm thấy bài viết" .
+          "</li>" .
+          "</ul>";
       }
     }
     return $output;
